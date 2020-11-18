d9e538d685c23f5edf62328de4b6b58b93e7ce3b
tangna <1095620719@qq.com>
2020-11-18 22:49:22 +0800

    add encipher and decipher test

--- test/cipher.js
+++ test/cipher.js
@@ -5,43 +5,112 @@ const config = require('../config/compress.json')
 const fs = require('fs')
 const path = require('path')
 
+const encipherPerFilePath = utils.getWholePath('./data/encipher-per-file')
+const encipherOneFilePath = utils.getWholePath('./data/encipher-one-file')
+const encipherOneDirPath = utils.getWholePath('./data/encipher-one-dir')
+const decipherPerFilePath = utils.getWholePath('./data/decipher-per-file')
+const decipherOneFilePath = utils.getWholePath('./data/decipher-one-file')
+const decipherOneDirPath = utils.getWholePath('./data/decipher-one-dir')
+
 describe('cipher', function () {
   before(async function () {
-    let { encipherDest } = config
-    let destPath = utils.getWholePath(encipherDest)
-    fs.readdirSync(destPath).forEach(file => {
-      fs.unlinkSync(path.join(destPath, './', file))
-    })
+    if (fs.existsSync(encipherPerFilePath)) {
+      fs.readdirSync(encipherPerFilePath).forEach(file => {
+        fs.unlinkSync(path.join(encipherPerFilePath, './', file))
+      })
+    }
+
+    if (fs.existsSync(encipherOneFilePath)) {
+      fs.readdirSync(encipherOneFilePath).forEach(file => {
+        fs.unlinkSync(path.join(encipherOneFilePath, './', file))
+      })
+    }
+
+    if (fs.existsSync(encipherOneDirPath)) {
+      fs.readdirSync(encipherOneDirPath).forEach(file => {
+        fs.unlinkSync(path.join(encipherOneDirPath, './', file))
+      })
+    }
   })
 
   it('encipher folder with perFile', async function () {
-    let { encipherSource, encipherDest, algorithm, password } = config
+    let { encipherSource, algorithm, password } = config
     let encipherMode = 'perFile'
     let sourcePath = utils.getWholePath(encipherSource)
-    let destPath = utils.getWholePath(encipherDest)
+    let destPath = encipherPerFilePath
     await cipher.encipherAdvance(sourcePath, destPath, algorithm, password, algorithm, encipherMode)
+
+    let files = fs.readdirSync(destPath)
+    expect(files.length).to.be.equal(2)
+    for (const f of files) {
+      expect(f.endsWith('.tz')).to.be.true
+    }
   })
 
   it('encipher folder with oneFile', async function () {
-    let { encipherSource, encipherDest, algorithm, password } = config
+    let { encipherSource, algorithm, password } = config
     let encipherMode = 'oneFile'
     let sourcePath = utils.getWholePath(encipherSource)
-    let destPath = utils.getWholePath(encipherDest)
+    let destPath = encipherOneFilePath
     await cipher.encipherAdvance(sourcePath, destPath, algorithm, password, algorithm, encipherMode)
+
+    let files = fs.readdirSync(destPath)
+    expect(files.length).to.be.equal(1)
+    for (const f of files) {
+      expect(f.endsWith('.tz')).to.be.true
+    }
   })
 
-  it.skip('encipher folder', async function () {
-    let { algorithm, password } = config
+  it('encipher folder with oneDir', async function () {
+    let { encipherSource, algorithm, password } = config
     let encipherMode = 'oneFile'
-    let sourcePath = utils.getWholePath('data/input/test')
-    let destPath = utils.getWholePath('data/encipher-folder')
+    let sourcePath = utils.getWholePath(encipherSource)
+    let destPath = encipherOneDirPath
     await cipher.encipherAdvance(sourcePath, destPath, algorithm, password, 9, encipherMode)
+
+    let files = fs.readdirSync(destPath)
+    expect(files.length).to.be.equal(1)
+    for (const f of files) {
+      expect(f.endsWith('.tz')).to.be.true
+    }
   })
 
-  it('decipher folder', async function () {
-    let { decipherSource, decipherDir, password } = config
-    let sourcePath = utils.getWholePath(decipherSource)
-    let destPath = utils.getWholePath(decipherDir)
+  it('decipher folder with perFile', async function () {
+    let { password } = config
+    let sourcePath = encipherPerFilePath
+    let destPath = decipherPerFilePath
     await cipher.decipherAdvance(sourcePath, destPath, password)
+
+    let files = fs.readdirSync(destPath)
+    expect(files.length).to.be.equal(2)
+    for (const f of files) {
+      expect(f.endsWith('.txt')).to.be.true
+    }
+  })
+
+  it('decipher folder with oneFile', async function () {
+    let { password } = config
+    let sourcePath = encipherOneFilePath
+    let destPath = decipherOneFilePath
+    await cipher.decipherAdvance(sourcePath, destPath, password)
+
+    let files = fs.readdirSync(destPath)
+    expect(files.length).to.be.equal(2)
+    for (const f of files) {
+      expect(f.endsWith('.txt')).to.be.true
+    }
+  })
+
+  it('decipher folder with oneDir', async function () {
+    let { password } = config
+    let sourcePath = encipherOneDirPath
+    let destPath = decipherOneDirPath
+    await cipher.decipherAdvance(sourcePath, destPath, password)
+
+    let files = fs.readdirSync(destPath)
+    expect(files.length).to.be.equal(2)
+    for (const f of files) {
+      expect(f.endsWith('.txt')).to.be.true
+    }
   })
 })
